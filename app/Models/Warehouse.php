31f2b0d5be29cc2197d69fab367b420058642686
Alex Bold <djalexbold@gmail.com>
2025-07-22 16:27:41 +0300

    Warehouses view

--- app/Models/Warehouse.php
+++ app/Models/Warehouse.php
@@ -2,12 +2,28 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\DB;
 
 class Warehouse extends Model
 {
     public $timestamps = false;
     use HasFactory;
+
     protected $fillable = ['name'];
+
+
+    public function index(): Response
+    {
+        return DB::table('warehouses')
+            ->orderBy('name')
+            ->paginate(5)
+            ->through(fn($warehouse) => [
+                'id' => $warehouse->id,
+                'name' => $warehouse->name,
+            ]);
+    }
 }
